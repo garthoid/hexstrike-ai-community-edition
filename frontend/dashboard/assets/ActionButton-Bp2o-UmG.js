409fe065c177e83da72c4fb89dfbcdc5bf7afb6f
CommonHuman-Lab <inboxshift@pm.me>
2026-08-13 23:10:00 +0200

    package updates

--- frontend/dashboard/assets/ActionButton-Bp2o-UmG.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./vendor-markdown-C0J_Dsog.js";var t=e();function n({variant:e=`default`,className:n=``,children:r,...i}){return(0,t.jsx)(`button`,{className:`action-button action-button--${e} press-feedback${n?` ${n}`:``}`,...i,children:r})}export{n as t};
---
+++ frontend/dashboard/assets/ActionButton-BrE4Os37.js
@@ -0,0 +1,1 @@
+import{r as e}from"./vendor-markdown-DQTur3rc.js";var t=e();function n({variant:e=`default`,className:n=``,children:r,...i}){return(0,t.jsx)(`button`,{className:`action-button action-button--${e} press-feedback${n?` ${n}`:``}`,...i,children:r})}export{n as t};
